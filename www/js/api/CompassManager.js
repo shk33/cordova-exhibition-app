2ff71fbedcd167faa305461e87df7e1e9f4bc159
Miguel Coronel <m.coronel.seg@gmail.com>
2015-08-02 18:26:30 -0500

    Repair compass vc

--- www/js/api/CompassManager.js
+++ www/js/api/CompassManager.js
@@ -1,26 +1,29 @@
-var CompassManager = (function () {
+//Singleton Object
+var CompassManager = (function () {     
   var instance;
-
-  function createObject () {
-    return {
-      startWatchHeading: function (callback) {
-        return navigator.compass.watchHeading(callback.onSuccess,
-            callback.onError, { frequency: 100});        
-      },
-      stopWatchHeading: function (watchID) {
-        if(watchID){
-          navigator.compass.clearWatch(watchID);
-        }
-      }
-    };
-  }
-
-  return  {
-    getInstance:  function  ()  {
-      if  (!instance) {
-        instance  = createObject();
-      }
-      return  instance;
-    }
+ 
+  function createObject() {
+      return {
+          startWatchHeading: function (callback) {
+              return navigator.compass.watchHeading(callback.onSuccess, 
+                                                    callback.onError, 
+                                                    {frequency: 100});
+          },
+          stopWatchHeading: function (watchID) {    
+              if (watchID) {
+                  navigator.compass.clearWatch(watchID);
+              }
+          }
+      };
   };
+ 
+  return {
+    getInstance: function () {
+      if (!instance) {
+          instance = createObject();
+      }
+ 
+      return instance;
+    }
+  }; 
 })();
